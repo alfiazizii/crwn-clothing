11883cfb1704acad595aada0be4651d714723d95
Alfi Azizi <azizi.alfi15@gmail.com>
2019-10-19 15:17:26 +0700

    fixing styling and missing className

--- src/components/collection-item/collection-item-component.jsx
+++ src/components/collection-item/collection-item-component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import CustomButton from '../custom-button/custom-button.component';
 import { addItem } from '../../redux/cart/cart.actions';
 
@@ -10,23 +11,32 @@ const CollectionItem = ({ item, addItem }) => {
 
   return (
     <div className='collection-item'>
-      <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div
+        className='image'
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
+      <CustomButton
+        className='custom-button'
+        onClick={() => addItem(item)}
+        inverted
+      >
         Add to cart
       </CustomButton>
     </div>
   );
 };
 
-const mapDispatchToPros = dispatch => ({
+const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
 
 export default connect(
   null,
-  mapDispatchToPros
+  mapDispatchToProps
 )(CollectionItem);
